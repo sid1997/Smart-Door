6d7507f2608742cdda41740000f8e2fad5fac4f3
Siddhant Jain <jainsiddhant29@gmail.com>
2018-11-01 23:03:17 +0530

    Add files via upload

--- SE_Facial_Recognition/fac_reg.py
+++ SE_Facial_Recognition/fac_reg.py
@@ -2,6 +2,7 @@ import face_recognition
 import sys
 import numpy as np
 import re
+import os
 
 #f = open("/home/sid/Desktop/SE_Facial_Recognition/true_encodings.txt","a+")
 
@@ -9,7 +10,9 @@ unknown_image = face_recognition.load_image_file(sys.argv[1])
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 #print(unknown_encoding)
 
-with open('./true_encodings.txt', 'r') as myfile:
+abs_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(str(abs_path)+'/true_encodings.txt', 'r') as myfile:
     data = myfile.read().replace('\n','')
 
 x = data.split('][')
