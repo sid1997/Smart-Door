cef00c7b2a6594a7bb86b8ba4d1bdddae60127ea
Siddhant Jain <jainsiddhant29@gmail.com>
2018-11-19 14:21:48 +0530

    Add files via upload

--- SE_Facial_Recognition/add.py
+++ SE_Facial_Recognition/add.py
@@ -1,17 +1,17 @@
 import os
+import sys
 import face_recognition
 #os.chdir("/home/sid/Desktop/SE_Facial_Recognition/known_images")
 
 f = open("./true_encodings.txt","a+")
 
-import os
-for file in os.listdir("./known_images"):
-    if file.endswith(".jpg"):
-        known_image = face_recognition.load_image_file("./known_images/"+file)
-        known_encoding = face_recognition.face_encodings(known_image)[0]
-        #print(known_encoding)
-        f.write(str(known_encoding)+"\n")
+unknown_image = face_recognition.load_image_file(sys.argv[1])
+try:
+    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    f.write(str(unknown_encoding)+"\n")
 
+except:
+    print("No face detected, try other image")
 #for img in glob.glob(".jpg"):
 #    known_image = face_recognition.load_image_file(img)
 #    known_encoding = face_recognition.face_encodings(known_image)[0]
--- SE_Facial_Recognition/fac_reg.py
+++ SE_Facial_Recognition/fac_reg.py
@@ -5,35 +5,37 @@ import re
 import os
 
 #f = open("/home/sid/Desktop/SE_Facial_Recognition/true_encodings.txt","a+")
+try:
+    unknown_image = face_recognition.load_image_file(sys.argv[1])
+    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-unknown_image = face_recognition.load_image_file(sys.argv[1])
-unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-#print(unknown_encoding)
+    abs_path = os.path.dirname(os.path.realpath(__file__))
 
-abs_path = os.path.dirname(os.path.realpath(__file__))
+    with open(str(abs_path)+'/true_encodings.txt', 'r') as myfile:
+        data = myfile.read().replace('\n','')
 
-with open(str(abs_path)+'/true_encodings.txt', 'r') as myfile:
-    data = myfile.read().replace('\n','')
-
-x = data.split('][')
-result = False
-x[0] = x[0][1:]
-x[-1] =x[-1][:-1]
-#print(x)
-for i in x:
-    t = re.split('  | ',i)
-    #print(t)
-    list = []
-    for num in t:
-        if(not num==''):
-            list.append(float(num))
-    test = np.asarray(list)
+        x = data.split('][')
+        result = False
+    x[0] = x[0][1:]
+    x[-1] =x[-1][:-1]
+    #print(x)
+    for i in x:
+        t = re.split('  | ',i)
+        #print(t)
+        list = []
+        for num in t:
+            if(not num==''):
+                list.append(float(num))
+                test = np.asarray(list)
     #print(test.size)
     #print(type(test))
     #print(type(unknown_encoding))
     #dist = face_recognition.face_distance([test],unknown_encoding)
     #print(dist)
-    bool = face_recognition.compare_faces([test],unknown_encoding,tolerance=0.45)
+        bool = face_recognition.compare_faces([test],unknown_encoding,tolerance=0.45)
     #print(bool[0])
-    result = result or bool[0]
-print(result)
+        result = result or bool[0]
+    print(result)
+
+except:
+    print("No face detected, try other image")
